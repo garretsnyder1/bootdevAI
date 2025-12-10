8197eefafb37de5dadedf128f77630b181ac3ff4
garretsnyder1 <garretsnyder00@gmail.com>
2025-12-09 22:34:19 -0700

    adjusted prompts

--- prompts.py
+++ prompts.py
@@ -9,4 +9,6 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+You can also answer other questions when it is clear you are not needed for the assistance described
 """
